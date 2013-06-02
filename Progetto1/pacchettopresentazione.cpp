4bbf3124daa9f462c7b6c886be55a6ffa9a47a0b
Giorgio <imatesiu@gmail.com>
2013-06-02 11:04:45 +0200

    fix pacchetto presentazione

--- Progetto1/pacchettopresentazione.cpp
+++ Progetto1/pacchettopresentazione.cpp
@@ -3,10 +3,10 @@
 
 pacchettopresentazione::pacchettopresentazione(void)
 {
-	data = gcnew Presentme();
-	data->NID_PACKET = 0;
-	data->L_PACKET = 0;
-	data->M_PORT =0;
+
+	NID_PACKET = 0;
+	L_PACKET = 0;
+	M_PORT =0;
 
 }
 
@@ -15,10 +15,10 @@ pacchettopresentazione::pacchettopresentazione(void)
 void pacchettopresentazione::serialize(byte *buffer)
 {
 	//push(buffer, data.NID_ENGINE, 24, 51);
-	push(buffer, data->NID_PACKET, 8, 75);
+	push(buffer, NID_PACKET, 8, 75);
 	setL_PACKET(getSize());
-	push(buffer, data->L_PACKET, 13, 83);
-	push(buffer, data->M_PORT, 32, 96);
+	push(buffer, L_PACKET, 13, 83);
+	push(buffer, M_PORT, 32, 96);
 
 }
 
@@ -29,21 +29,19 @@ void pacchettopresentazione::deserialize(byte *buff)
 
 
 	//data.NID_ENGINE = pop(buff, 24, 51);
-	data->NID_PACKET = pop(buff, 8, 75);
-	data->L_PACKET = pop(buff, 13, 83);
-	data->M_PORT = pop(buff, 32, 96);
+	NID_PACKET = pop(buff, 8, 75);
+	L_PACKET = pop(buff, 13, 83);
+	M_PORT = pop(buff, 32, 96);
 
 }
 
-pacchettopresentazione::~pacchettopresentazione(void)
-{
-}
+
 
 String ^pacchettopresentazione::ToString() {
 	System::String ^out;
 
-	out = out+"NID_PACKET: "+data->NID_PACKET+";";
-	out = out+"L_PACKET: "+data->L_PACKET+";";
-	out = out+"M_PORT: "+data->M_PORT+";";
+	out = out+"NID_PACKET: "+NID_PACKET+";";
+	out = out+"L_PACKET: "+L_PACKET+";";
+	out = out+"M_PORT: "+M_PORT+";";
 	return out;
 }
--- Progetto1/pacchettopresentazione.h
+++ Progetto1/pacchettopresentazione.h
@@ -1,6 +1,6 @@
 #pragma once
 #include "utility.h"
-#include "Presentme.h"
+
 
 /*-----------------------------------------------------------------------------------------------
 L'ATS riceve dal treno dei messaggi di 
@@ -8,7 +8,9 @@ L'ATS riceve dal treno dei messaggi di
 
 ref class pacchettopresentazione
 {
-	Presentme ^data;
+	unsigned int NID_PACKET;
+	unsigned int L_PACKET;
+	unsigned int M_PORT;
 public:
 	pacchettopresentazione(void);
 	// funzione che restituisce la dimensione (ideale, non quella dovuta agli allineamenti 
@@ -18,17 +20,17 @@ public:
 	// funzioni di interfaccia set e get per ogni campo dati del pacchetto
 
 
-	void setNID_PACKET(int N){data->NID_PACKET = N;};
-	int getNID_PACKET(){return data->NID_PACKET;};
-	void setL_PACKET(int L){data->L_PACKET = L;};
-	int getL_PACKET(){return data->L_PACKET;};
-	void setM_PORT(int Q){data->M_PORT = Q;};
-	int getM_PORT(){return data->M_PORT;};
+	void setNID_PACKET(int N){NID_PACKET = N;};
+	int getNID_PACKET(){return NID_PACKET;};
+	void setL_PACKET(int L){L_PACKET = L;};
+	int getL_PACKET(){return L_PACKET;};
+	void setM_PORT(int Q){M_PORT = Q;};
+	int getM_PORT(){return M_PORT;};
 	// metodi per la serializzazione e deserializzazione del messaggio
 	// il buffer di byte deve essere stato precedentemente correttamente allocato.
 	void serialize(byte *buff);
 	void deserialize(byte *buff);
-	~pacchettopresentazione(void);
+	
 
 	virtual System::String ^ToString() override;
 };
